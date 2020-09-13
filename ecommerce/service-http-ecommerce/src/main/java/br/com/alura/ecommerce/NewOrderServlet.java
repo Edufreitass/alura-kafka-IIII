0f30fb7f598272443af17648837c67b76eae27a5
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-09-12 21:49:05 -0300

    Aula 01 - Micro serviços de email e fast delegate real

--- ecommerce/service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ ecommerce/service-http-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -14,13 +14,11 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderServlet extends HttpServlet {
 
     private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
-    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
     @Override
     public void destroy() {
         super.destroy();
         orderDispatcher.close();
-        emailDispatcher.close();
     }
 
     @Override
@@ -39,21 +37,13 @@ public class NewOrderServlet extends HttpServlet {
                     new CorrelationId(NewOrderServlet.class.getSimpleName()),
                     order);
 
-            var emailCode = "Thank you for your order! We are processing your order!";
-            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email,
-                    new CorrelationId(NewOrderServlet.class.getSimpleName()),
-                    emailCode);
-
             System.out.println("New order sent successfully.");
             resp.setStatus(HttpServletResponse.SC_OK);
             resp.getWriter().println("New order sent");
 
-        } catch (ExecutionException e) {
-            throw new ServletException(e);
-        } catch (InterruptedException e) {
+        } catch (ExecutionException | InterruptedException e) {
             throw new ServletException(e);
         }
-
 
     }
 }
