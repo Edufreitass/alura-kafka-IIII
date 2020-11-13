437bd3d1d8f82aae2d389b7e0641e056f2d89568
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-13 15:11:45 -0300

    Aula 05 - Id natural e idempotência no banco

--- ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
+++ ecommerce/service-email-new-order/src/main/java/br/com/alura/ecommerce/EmailNewOrderService.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ExecutionException;
 public class EmailNewOrderService implements ConsumerService<Order> {
 
     public static void main(String[] args) {
-        new ServiceRunner(EmailNewOrderService::new).start(1);
+        new ServiceRunner<>(EmailNewOrderService::new).start(1);
     }
 
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
